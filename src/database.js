70109b9b558239dbae32df7dd8679194e1ca3bb1
Andrés Gallo <andres.gallo@monolegal.co>
2021-03-10 23:57:35 -0500

    Cambio package

--- src/database.js
+++ src/database.js
@@ -2,7 +2,13 @@ const mysql = require('mysql');
 const {promisify} = require('util');
 const log4js = require('log4js');
 
-let logger = log4js.getLogger('index.js');
+log4js.configure({
+    appenders: {cheese: {type: "file", filename: "cheese.log"}},
+    categories: {default: {appenders: ["cheese"], level: "all"}}
+});
+
+
+let logger = log4js.getLogger('cheese');
 logger.level = 'all';
 
 const {database} = require('./config');
@@ -12,7 +18,7 @@ const pool = mysql.createPool(database);
 pool.getConnection((err, connection) => {
     if (err) {
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-          logger.error('Database connection was closed.');
+            logger.error('Database connection was closed.');
         }
         if (err.code === 'ER_CON_COUNT_ERROR') {
             logger.error('Database has to many connections');
@@ -25,6 +31,8 @@ pool.getConnection((err, connection) => {
     if (connection) connection.release();
     logger.info('DB is Connected');
 
+    logger.error('DB is Connected');
+    logger.warn('DB is Connected');
     return;
 });
 
